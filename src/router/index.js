7bf9acff597bb774354b6825746e93ca9a058bb4
Arinda Rahma Dini <arindarahmadiny@gmail.com>
2021-02-18 22:27:51 +0700

    vue client

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Game from '../views/Game.vue'
+import Welcome from '../views/Welcome.vue'
+import Gameover from '../views/Gameover.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Welcome',
+    component: Welcome
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/game',
+    name: 'Game',
+    component: Game
+  },
+  {
+    path: '/gameover',
+    name: 'Gameover',
+    component: Gameover
   }
 ]
 
---
+++ src/views/Gameover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Gameover">
+    <div class="card-gameover mx-auto" style="width: 30rem;">
+      <div class="card-body">
+        <img src="https://cdn1.iconfinder.com/data/icons/video-game-14/48/video_game_pixel_perfect_icons_25-game-over-512.png" class="card-img-top" alt="...">
+        <br><br>
+        <h1 class="win-text">TANGAN KIRI WIN!</h1>
+        <br>
+        <button type="button" class="btn btn-danger btn-block">EXIT</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameOver'
+}
+</script>
+
+<style scoped>
+ .card-gameover {
+      margin-top: 4%;
+      background-color: #f1c40f !important;
+    }
+</style>
